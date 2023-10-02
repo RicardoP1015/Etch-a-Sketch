7a5249eedc59a2feed8bda06e0ccd59533cdfc93
RicardoP <picoricardo3d@outlook.com>
2023-10-01 21:11:10 -0400

    Add a way to change the etchSketch gird size and reworked a function
    on the UI made a button, number input and another button to look like
    the original Etch a Sketch toy
    
    reworked createEtchGrid function to only make the grid and made
    createBlock function to make the blocks separately

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const etchSketch = document.querySelector('.etch-sketch-container');
 
+const createBlock = (blockSize) => {
+    const newBlock = document.createElement('div');
+    newBlock.classList.add('block');
+    newBlock.style.width = `${blockSize}px`;
+    newBlock.style.height = `${blockSize}px`;
+    return newBlock;
+}
+
 const createEtchGrid = (amount) => {
+    const etchSize = etchSketch.clientWidth / amount;
     for (let i = 0; i < amount; i++) {
-        const newBlock = document.createElement('div');
-        newBlock.classList.add('block')
-        etchSketch.appendChild(newBlock);
+        for (let j = 0; j < amount; j++) {
+            etchSketch.appendChild(createBlock(etchSize));
+        }
     }
 }
 
-createEtchGrid(1)
+createEtchGrid(4);
