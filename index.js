839d15130253873ce92ecc55e9d7017066e72d38
RicardoP <picoricardo3d@outlook.com>
2023-10-02 04:25:02 -0400

    EventListeners to change blocks color when hovered and update to the UI
    reworked createEtchGrid function so it has EventListener for mouseover
    to change the background color to the selected color by default black,
    the EventListener was add to this function so that when you switch to
    a bigger board or smaller the blocks can still change there color
    
    the UI grid selector buttons now have hover effects for revealing the
    arrow direction using mouseover EventListener so that the hover effect
    happens when you hover over the button not just the arrow element as
    well to look more uniform

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const etchSketch = document.querySelector('.etch-sketch-container');
 const gridSizeDisplay = document.querySelector('.grid-size')
-const subBtn = document.querySelector('.minus');
-const addBtn = document.querySelector('.add');
+const subBtn = document.getElementById('sub-btn');
+const addBtn = document.getElementById('add-btn');
 const restBtn = document.getElementById('rest-btn');
+const clearBtn = document.getElementById('clear-btn');
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+
+let selectedColor = 'black';
 let currentGridSize = 16;
 const defaultGridSize = currentGridSize;
-
 
 const createBlock = (blockSize) => {
     const newBlock = document.createElement('div');
@@ -13,51 +17,76 @@ const createBlock = (blockSize) => {
     newBlock.style.width = `${blockSize}px`;
     newBlock.style.height = `${blockSize}px`;
     return newBlock;
-}
+};
 
 const createEtchGrid = (amount) => {
     const etchSize = etchSketch.clientWidth / amount;
     for (let i = 0; i < amount; i++) {
         for (let j = 0; j < amount; j++) {
-            etchSketch.appendChild(createBlock(etchSize));
-        }
-    }
-}
+            const block = createBlock(etchSize);
+            block.addEventListener('mouseover', () => {
+                block.style.backgroundColor = selectedColor;
+            });
+            etchSketch.appendChild(block);
+        };
+    };
+};
 
 const clearGrid = () => {
     while (etchSketch.firstChild) {
         etchSketch.removeChild(etchSketch.lastChild);
-    }
-    createEtchGrid(currentGridSize)
-} 
+    };
+    createEtchGrid(currentGridSize);
+};
 
 const subValue = () => {
     if (currentGridSize > 4) {
         currentGridSize -= 4;
-    }
-    clearGrid()
+    };
+    clearGrid();
     gridSizeDisplay.textContent = `${currentGridSize}x${currentGridSize}`;
     createEtchGrid(currentGridSize);
     clearGrid();
-}
+};
 
 const addValue = () => {
     if (currentGridSize < 100) {
-        currentGridSize += 4
-    }
+        currentGridSize += 4;
+    };
     gridSizeDisplay.textContent = `${currentGridSize}x${currentGridSize}`;
     createEtchGrid(currentGridSize);
     clearGrid();
-}
+};
 
 const restGrid = () => {
     currentGridSize = defaultGridSize;
     gridSizeDisplay.textContent = `${currentGridSize}x${currentGridSize}`;
-    clearGrid()
-}
+    clearGrid();
+};
+
+const HoverEffect = (side) => {
+    side.style.color = 'black';
+};
+
+const removeHoverEffect = (side) => {
+    side.style.color = 'transparent';
+};
 
 createEtchGrid(defaultGridSize);
 
 subBtn.addEventListener('click', subValue);
+subBtn.addEventListener('mouseover', () => {
+    HoverEffect(left);
+});
+subBtn.addEventListener('mouseout', () => {
+    removeHoverEffect(left);
+});
 addBtn.addEventListener('click', addValue);
+addBtn.addEventListener('mouseover', () => {
+    HoverEffect(right);
+});
+addBtn.addEventListener('mouseout', () => {
+    removeHoverEffect(right);
+});
 restBtn.addEventListener('click', restGrid);
+clearBtn.addEventListener('click', clearGrid);
